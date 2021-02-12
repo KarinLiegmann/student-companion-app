30d0b7f5d3f9b0f7fedd3a732584bfe6934e09f8
Stefan Korder <xtybevip@googlemail.com>
2021-02-12 13:32:05 +0100

    improved buddy and teams page

--- src/js/components/teams.js
+++ src/js/components/teams.js
@@ -1,82 +1,37 @@
+let people = [];
 
-/* const people = ['Andre T', 'André W.', 'Anita', 'Calvin', 'Daniel', 'Emanuel', 'Karin', 'Lea', 'Maize', 'Mali', 'Nicole', 'Philipp', 'Simon', 'Sophie', 'Stefan', 'Verena'];
-const teamsection = document.body.querySelector('.teams'); */
-
-const people = [];
+const teamsection = document.body.querySelector('.teams');
 
 fetch('https://muc-2020-w1-student-api.vercel.app/api/teams')
     .then(res => res.json())
     .then(persons => {
-        people.push(persons)
+        people = persons
+        people.forEach(renderPerson)
+        console.log(persons)
     })
     .catch(error => console.error(error.message))
 
-console.log(people)
 
 
 
-function renderPerson(people, index) {
-
-
-
-
-    const teamsection = document.body.querySelector('.teams');
-    const listSection = document.createElement('section')
-    listSection.classList.add('list')
-    teamsection.appendChild(listSection)
-    const listHeadline = document.createElement('h2')
-    listHeadline.classList.add('list__headline')
-    listSection.appendChild(listHeadline)
-    listHeadline.innerText = 'Team ' + index
-    const listContent = document.createElement('ul')
-    listContent.classList.add('list__content')
-    listHeadline.appendChild(listContent)
-
-
-    element.innerHTML = `<section class="list">
-    <h2 class="list__headline">Team 1</h2>
-    <ul class="list__content">
-        <li class="name">${people[0]}</li>
+function renderPerson(person, index) {
+        const teamList = createElement('section', 'list');
+        teamsection.appendChild(teamList)
+        const teamHeadline = createElement('h2', 'list__headline');
+        teamList.appendChild(teamHeadline)
+        const teamUnList = createElement('ul', 'list__content');
+        teamList.appendChild(teamUnList)
        
-    </ul>
-    </section>
+        teamHeadline.innerText = 'Team ' + (index + 1)
+        person.forEach((pers) => {
+            const teamListItem = createElement('li', 'name');
+            teamUnList.appendChild(teamListItem)
+            teamListItem.innerText = pers
+          })
+    }
 
-
-    `
-    todoList.appendChild(element)
+function createElement(type, classe) {
+    const element = document.createElement(type);
+    element.classList.add(classe);
+    return element;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-teamsection.innerHTML = `
-<section class="list">
-<h2 class="list__headline">Team 2</h2>
-<ul class="list__content">
-    <li class="name">${people[5]}</li>
-    <li class="name">${people[6]}</li>
-    <li class="name">${people[7]}</li>
-    <li class="name">${people[8]}</li>
-    <li class="name">${people[9]}</li>
-</ul>
-</section>`
-
-
-
-
-
-
-
-
-
-
-
-
