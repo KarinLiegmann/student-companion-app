c5e23e3bffe56902376f4e192cfd8a8b41b22b62
Karin Liegmann <karin.liegmann@googlemail.com>
2021-02-12 10:24:02 +0100

    fetched buddy-array

--- src/js/components/teams.js
+++ src/js/components/teams.js
@@ -1,18 +1,63 @@
 
-const people = ['Andre T', 'André W.', 'Anita', 'Calvin', 'Daniel', 'Emanuel', 'Karin', 'Lea', 'Maize', 'Mali', 'Nicole', 'Philipp', 'Simon', 'Sophie', 'Stefan', 'Verena'];
-const teamsection = document.body.querySelector('.teams');
+/* const people = ['Andre T', 'André W.', 'Anita', 'Calvin', 'Daniel', 'Emanuel', 'Karin', 'Lea', 'Maize', 'Mali', 'Nicole', 'Philipp', 'Simon', 'Sophie', 'Stefan', 'Verena'];
+const teamsection = document.body.querySelector('.teams'); */
+
+const people = [];
+
+fetch('https://muc-2020-w1-student-api.vercel.app/api/teams')
+    .then(res => res.json())
+    .then(persons => {
+        people.push(persons)
+    })
+    .catch(error => console.error(error.message))
+
+console.log(people)
 
 
-teamsection.innerHTML = `<section class="list">
-<h2 class="list__headline">Team 1</h2>
-<ul class="list__content">
-    <li class="name">${people[0]}</li>
-    <li class="name">${people[1]}</li>
-    <li class="name">${people[2]}</li>
-    <li class="name">${people[3]}</li>
-    <li class="name">${people[4]}</li>
-</ul>
-</section>
+
+function renderPerson(people, index) {
+
+
+
+
+    const teamsection = document.body.querySelector('.teams');
+    const listSection = document.createElement('section')
+    listSection.classList.add('list')
+    teamsection.appendChild(listSection)
+    const listHeadline = document.createElement('h2')
+    listHeadline.classList.add('list__headline')
+    listSection.appendChild(listHeadline)
+    listHeadline.innerText = 'Team ' + index
+    const listContent = document.createElement('ul')
+    listContent.classList.add('list__content')
+    listHeadline.appendChild(listContent)
+
+
+    element.innerHTML = `<section class="list">
+    <h2 class="list__headline">Team 1</h2>
+    <ul class="list__content">
+        <li class="name">${people[0]}</li>
+       
+    </ul>
+    </section>
+
+
+    `
+    todoList.appendChild(element)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+teamsection.innerHTML = `
 <section class="list">
 <h2 class="list__headline">Team 2</h2>
 <ul class="list__content">
